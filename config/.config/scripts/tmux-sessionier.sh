bedc60649ed68235bde42a73ae4718fde97f6f7c
Gabriel Rocha <rochafrgabriel@gmail.com>
2025-02-16 02:53:49 -0300

    new dir into tmux-sessionier.sh

--- config/.config/scripts/tmux-sessionier.sh
+++ config/.config/scripts/tmux-sessionier.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/.config ~/github ~/github/adapcon ~/github/adapcon/amc-simplificamais/microservices ~/github/adapcon/amc-simplificamais/app/src/modules ~/github/personal ~/github/catolica ~/ -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/.config ~/github ~/github/galvitech ~/github/adapcon ~/github/adapcon/amc-simplificamais/microservices ~/github/adapcon/amc-simplificamais/app/src/modules ~/github/personal ~/github/catolica ~/ -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
